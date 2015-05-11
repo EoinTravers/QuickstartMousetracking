4f737aac5264f0b8769ce5a7c226fd6edc9d82dd
Eoin Travers <eoint02@gmail.com>
2015-05-11 12:40:25 +0100

    Inital

---
+++ procedure/Run Experiment.bat
@@ -0,0 +1,24 @@
+@echo off
+    
+set opensesame=C:\Users\40027000\Desktop\Software\OpenSesame\opensesame_2.8.3-win32-1\opensesamerun.exe
+
+rem Read subject number from the file subject_nr.txt    
+set number_path=%~dp0\subject_nr.txt
+if exist %number_path% for /f "delims=" %%i in (%number_path%) do set /A subject_nr=%%i
+echo Subject number = %subject_nr%.
+
+rem Increment subject number, and save it to the file
+set /A next_subject = %subject_nr% + 1
+echo %next_subject% > %number_path%
+
+rem Make sure the 'data' folder exists
+if not exist %~dp0data mkdir %~dp0data
+
+rem Run experiment, with set subject number, and save location, in fullscreen mode    
+echo Starting experiment...
+%opensesame% %~dp0experiment.opensesame -s %subject_nr% ^
+    -l %~dp0data\subject_%subject_nr%.csv -f > NUL
+
+echo Experiment complete!
+echo Goodbye.
+pause 
---
+++ results/process.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+import os 
+import glob
+
+try:
+    import numpy as np # Numeric calculation
+    import pandas as pd # General purpose data analysis library
+    import squeak # For mouse data
+except:
+    raise Exception("\
+Whoops, you're missing some of the dependencies you need to run this script.\n\
+You need to have numpy, pandas, and squeak installed.")
+
+this_dir = os.path.abspath('.')
+print "Running in %s\n\
+Checking for .csv files in %s" % (this_dir, os.path.join(this_dir, 'data'))
+
+datafiles = glob.glob('data/*.csv')
+print "%i files found:" % len(datafiles)
+print '\n'.join(datafiles)
+
+
+print "\nProcessing..."
+data = pd.concat(
+    [pd.DataFrame(pd.read_csv(datafile)) 
+     for datafile in datafiles])
+
+data['t'] = data.tTrajectory.map(squeak.list_from_string)
+data['x'] = data.xTrajectory.map(squeak.list_from_string)
+data['y'] = data.yTrajectory.map(squeak.list_from_string)
+
+data['y'] = data.y * -1 # Reverse y axis
+data['x'] = data.x.map(squeak.remap_right) # Flip the leftward responses
+data['x'] = data.x.map(squeak.normalize_space)
+data['y'] = data.y.map(squeak.normalize_space) * 1.5
+
+
+# Normalized time
+data['nx'], data['ny'] = zip(*[squeak.even_time_steps(x, y, t) 
+                               for x, y, t, in zip(data.x, data.y, data.t)])
+
+# Real time
+max_time = 5000 # Alternatively, max_time = data.rt.max()
+data['rx'] = [squeak.uniform_time(x, t, max_duration=5000) for x, t in zip(data.x, data.t)]
+data['ry'] = [squeak.uniform_time(y, t, max_duration=5000) for y, t in zip(data.y, data.t)] 
+
+# Mouse Stats
+data['md'] = data.apply(lambda trial: squeak.max_deviation(trial['nx'], trial['ny']), axis=1)
+data['auc'] = data.apply(lambda trial: squeak.auc(trial['nx'], trial['ny']), axis=1)
+data['xflips'] = data.nx.map(squeak.count_x_flips)
+data['init_time'] = data.ry.map(lambda y: y.index[np.where(y > .05)][0])
+
+
+# Seperate data frames
+nx = pd.concat(list(data.nx), axis=1).T
+ny = pd.concat(list(data.ny), axis=1).T
+
+rx = pd.concat(list(data.rx), axis=1).T
+ry = pd.concat(list(data.ry), axis=1).T
+
+redundant = ['xTrajectory', 'yTrajectory', 'tTrajectory',
+             'x', 'y', 't', 'nx', 'ny', 'rx', 'ry']
+data = data.drop(redundant, axis=1)
+
+print "Done!\n"
+
+# Save data
+data.to_csv('processed.csv', index=False)
+print "Summary statistics saved to %s" % os.path.join(this_dir, 'processed.csv')
+nx.to_csv('nx.csv', index=False)
+ny.to_csv('ny.csv', index=False)
+rx.to_csv('rx.csv', index=False)
+ry.to_csv('ry.csv', index=False)
+
+for n in ['nx', 'ny', 'rx', 'ry']:
+    print "Mouse trajectories saved to %s.csv" % os.path.join(this_dir, n)
